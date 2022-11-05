f41ef51acd5bb43ed74315d41e3aa4fa7a58e727
enriquedlh97 <enriquedlh97@hotmail.com>
2022-11-05 15:08:28 -0600

    feat: Adds task creation functionality.

--- backend/base/urls.py
+++ backend/base/urls.py
@@ -1,9 +1,10 @@
 from django.urls import URLPattern, path
 
-from .views import TaskDetail, TaskList
+from .views import TaskCreate, TaskDetail, TaskList
 
 urlpatterns: list[URLPattern] = [
     path("", TaskList.as_view(), name="tasks"),
     # The view here looks for a primary key referencing a specific task.
     path("task/<int:pk>/", TaskDetail.as_view(), name="task"),
+    path("task-create/", TaskCreate.as_view(), name="task-create"),
 ]
--- backend/base/views.py
+++ backend/base/views.py
@@ -1,5 +1,9 @@
-from django.db.models.base import ModelBase
+from typing import Callable, Literal, Optional, Sequence, Type
+
+from django.db.models import Model
+from django.urls import reverse_lazy
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView
 from django.views.generic.list import ListView
 
 from .models import Task
@@ -9,12 +13,12 @@ class TaskList(ListView):
     """Task List View: Contains all the tasks displayed.
 
     Attributes:
-        model (ModelBase): Model for the view.
+        model (Model): Model for the view.
         context_object_name (str): Name to reference the object within
             the 'task_list.html' template.
     """
 
-    model: ModelBase = Task
+    model: Type[Model] = Task
     context_object_name: str = "tasks"
 
 
@@ -22,13 +26,31 @@ class TaskDetail(DetailView):
     """Task Detail View: Shows the details for a particular Task.
 
     Attributes:
-        model (ModelBase): Model for the view.
+        model (Model): Model for the view.
         context_object_name (str): Name to reference the object within
             the 'task_detail.html' template.
         template_name (str): Name to use for the template instead of
             'task_detail.html'.
     """
 
-    model: ModelBase = Task
-    context_object_name: str = "task"
+    model: Type[Model] = Task
+    content_type: Optional[str] = "task"
     template_name: str = "base/task.html"
+
+
+class TaskCreate(CreateView):
+    """Task Create View: Creates a Task.
+
+    Attributes:
+        model (Model): Model for the view.
+        fields (`list` of `str` or `literal`): Fields to be shown in the
+            form.
+        success_url (`str` or `callable`): Function to be used when the
+            form is completed.
+    """
+
+    model: Type[Model] = Task
+    # Specifies fields to show in form, this shows all fields from the model
+    fields: Optional[Sequence[str] | Literal["__all__"]] = "__all__"
+    # Redirects back to the list of tasks once the new task is created
+    success_url: Optional[str | Callable[[str], str]] = reverse_lazy("tasks")
