bcb04b4a49b71fcc68239ccd495921f27dce4d7c
enriquedlh97 <enriquedlh97@hotmail.com>
2022-11-05 14:23:30 -0600

    feat: Adds task detail view.

--- backend/base/urls.py
+++ backend/base/urls.py
@@ -1,7 +1,9 @@
 from django.urls import URLPattern, path
 
-from .views import TaskList
+from .views import TaskDetail, TaskList
 
 urlpatterns: list[URLPattern] = [
     path("", TaskList.as_view(), name="tasks"),
+    # The view here looks for a primary key referencing a specific task.
+    path("task/<int:pk>/", TaskDetail.as_view(), name="task"),
 ]
--- backend/base/views.py
+++ backend/base/views.py
@@ -1,9 +1,34 @@
 from django.db.models.base import ModelBase
+from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
 
 from .models import Task
 
 
 class TaskList(ListView):
+    """Task List View: Contains all the tasks displayed.
+
+    Attributes:
+        model (ModelBase): Model for the view.
+        context_object_name (str): Name to reference the object within
+            the 'task_list.html' template.
+    """
+
     model: ModelBase = Task
     context_object_name: str = "tasks"
+
+
+class TaskDetail(DetailView):
+    """Task Detail View: Shows the details for a particular Task.
+
+    Attributes:
+        model (ModelBase): Model for the view.
+        context_object_name (str): Name to reference the object within
+            the 'task_detail.html' template.
+        template_name (str): Name to use for the template instead of
+            'task_detail.html'.
+    """
+
+    model: ModelBase = Task
+    context_object_name: str = "task"
+    template_name: str = "base/task.html"
